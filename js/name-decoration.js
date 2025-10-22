b2782c971096c9789b9f6eb8d6e0ae8eda785451
abu7amza99-ctrl <abu.7amza99@gmail.com>
2025-10-22 12:31:35 +0300

    Update name-decoration.js

--- js/name-decoration.js
+++ js/name-decoration.js
@@ -1,1 +1,21 @@
+const menuBtn = document.getElementById("menuBtn");
+const sidePanel = document.getElementById("sidePanel");
+const closePanel = document.getElementById("closePanel");
+const typeButton = document.getElementById("typeButton");
+const typeMenu = document.getElementById("typeMenu");
 
+// فتح وإغلاق اللوحة الجانبية
+menuBtn.addEventListener("click", () => {
+  sidePanel.classList.add("open");
+});
+
+closePanel.addEventListener("click", () => {
+  sidePanel.classList.remove("open");
+});
+
+// زر اختيار نوع الزخرفة
+typeButton.addEventListener("click", () => {
+  typeMenu.classList.toggle("hidden");
+});
+
+// لاحقًا نضيف الأحداث الخاصة بخيارات الزخرفة
